d1daeec78e7f3720200d25007c012a06d817d081
him-raw99 <him.raw.official1@gmail.com>
2024-03-27 17:06:39 +0530

    fix: minor bug

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { errorRouter } from "./error";
 import { userRouter } from "./user";
+import { orderRouter } from "./order";
 
 const router = Router();
 router.use("/", userRouter);
+router.use("/", orderRouter);
 router.use("/*", errorRouter);
 
 export { router };

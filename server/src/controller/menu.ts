d7c8e3ab0d613c280c2c5222155c241474e6d03e
him-raw99 <him.raw.official1@gmail.com>
2024-04-21 13:49:17 +0530

    add: delete menu + minor bugs

--- server/src/controller/menu.ts
+++ server/src/controller/menu.ts
@@ -3,55 +3,63 @@ import { APIResponse } from "../utils/responseHandeler";
 import { Mongoose } from "mongoose";
 import { Menu } from "../models/menu";
 import { APIError } from "../utils/errorHandler";
-async function menuDetails(req,res,next) {
-    const canteen=req.query.canteen;
+async function menuDetails(req, res, next) {
+    const canteen = req.query.canteen;
     console.log(canteen);
-    if(canteen){
-        try{
-            const Items=await Menu.find({canteen});
-            res.json(APIResponse(`menu of the ${canteen}`, 200,Items, true)).status(200);
-        }
-        catch(eror){
+    if (canteen) {
+        try {
+            const Items = await Menu.find({ canteen });
+            res.json(
+                APIResponse(`menu of the ${canteen}`, 200, Items, true)
+            ).status(200);
+        } catch (eror) {
             next(eror);
         }
-    }
-    else{
-        try{
-            const Items=await Menu.find();
-            res.json(APIResponse(`menu of  all the canteen`, 200,Items, true)).status(200);
-        }
-        catch(eror){
+    } else {
+        try {
+            const Items = await Menu.find();
+            res.json(
+                APIResponse(`menu of  all the canteen`, 200, Items, true)
+            ).status(200);
+        } catch (eror) {
             next(eror);
         }
     }
 }
-async function addMenu(req,res,next) {
-    const payload=req.body;
-    if(payload){
-        try{
-            const Item=await Menu.create(payload);
-            res.json(APIResponse(`Created the Item`, 200,Item, true)).status(200);
-        }
-        catch(error){
+async function addMenu(req, res, next) {
+    const payload = req.body;
+    if (payload) {
+        try {
+            const Item = await Menu.create(payload);
+            res.json(APIResponse(`Created the Item`, 200, Item, true)).status(
+                200
+            );
+        } catch (error) {
             next(error);
         }
-    }
-    else{
-        throw new APIError(
-            "Detail Not Given!!",
-            400
-        );
+    } else {
+        throw new APIError("Detail Not Given!!", 400);
     }
 }
-async function updateMenu(req,res,next) {
-   const payload=req.body;
-   const id=req.body._id;
-   try{
-    const data=await Menu.findByIdAndUpdate({_id:id},payload,{new:true});
-    res.json(APIResponse(`update the item`, 200,data, true)).status(200);
-   }
-   catch(error){
-    next(error);
-   }
+async function updateMenu(req, res, next) {
+    const payload = req.body;
+    const id = req.body._id;
+    try {
+        const data = await Menu.findByIdAndUpdate({ _id: id }, payload, {
+            new: true,
+        });
+        res.json(APIResponse(`update the item`, 200, data, true)).status(200);
+    } catch (error) {
+        next(error);
+    }
 }
-export const menuController={addMenu,menuDetails,updateMenu};
+async function deleteMenu(req, res, next) {
+    const id = req.body._id;
+    try {
+        const data = await Menu.findByIdAndDelete(id);
+        res.json(APIResponse(`deleted the item`, 200, data, true)).status(200);
+    } catch (error) {
+        next(error);
+    }
+}
+export const menuController = { addMenu, menuDetails, updateMenu, deleteMenu };
--- server/src/models/order.ts
+++ server/src/models/order.ts
@@ -20,7 +20,7 @@ const orderSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    opt: {
+    otp: {
         type: Number,
         required: true,
     },

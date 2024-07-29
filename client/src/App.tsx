35728d2bf792feddc83eb3dbb65c9650492e9435
AlphaCupcake10 <alphacupcake@outlook.com>
2024-07-29 14:08:20 +0530

    chore: Add backend boot up message when backend is not active

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,13 +6,14 @@ import DashboardPage from "./pages/DashboardPage";
 import { useReduxAction, useReduxState } from "./hooks/UseRedux";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Admin from "./pages/admin/Admin";
 import axios from "./axios";
 import OrdersPage from "./pages/OrdersPage";
 import DefaultRoute from "./pages/DefaultRoute";
 
 function App() {
+    const [isBackendActive, setIsBackendActive] = useState(false);
     //TODO CHANGE TO REDUX STATE
     const { isSignedIn } = useReduxState();
     const { setIsSignedIn, setLoginData } = useReduxAction();
@@ -37,9 +38,13 @@ function App() {
         try {
             const resp = await axios.get("/menu");
             if (resp.status == 200)
+            {
                 setMenu(resp.data.data);
+                setIsBackendActive(true);    
+            }
         }
         catch (e: any) {
+            console.log(e);
             toast.error(e.message, {
                 position: "bottom-right",
             });
@@ -49,26 +54,35 @@ function App() {
     return (
         <>
             <ToastContainer />
-            <Routes>
-                {
-                    isSignedIn ?
-                        (
-                            <>
-                                <Route path="/" element={<DashboardPage />} />
-                                <Route path='/orders' element={<OrdersPage/>} />
-                            </>
-                        ) :
-                        (
-                            <>
-                                <Route path="/" element={<LandingPage />} />
-                                <Route path="/signin" element={<SignInPage />} />
-                                <Route path="/signup" element={<SignUpPage />} />
-                            </>
-                        )
-                }
-                <Route path="/*" element={<DefaultRoute/>} />
-                <Route path="/admin/*" element={<Admin />} />
-            </Routes>
+            {
+                isBackendActive ? (
+                    <Routes>
+                    {
+                        isSignedIn ?
+                            (
+                                <>
+                                    <Route path="/" element={<DashboardPage />} />
+                                    <Route path='/orders' element={<OrdersPage/>} />
+                                </>
+                            ) :
+                            (
+                                <>
+                                    <Route path="/" element={<LandingPage />} />
+                                    <Route path="/signin" element={<SignInPage />} />
+                                    <Route path="/signup" element={<SignUpPage />} />
+                                </>
+                            )
+                    }
+                    <Route path="/*" element={<DefaultRoute/>} />
+                    <Route path="/admin/*" element={<Admin />} />
+                </Routes>
+                ) : (
+                    <div className="flex flex-col items-center justify-center h-screen">
+                        <h1 className="text-3xl font-medium">Booting up the backend...</h1>
+                        <h1 className="text-sm mt-4">It's hosted on render, visit after a few seconds.</h1>
+                    </div>
+                ) 
+            }
         </>
     );
 }
